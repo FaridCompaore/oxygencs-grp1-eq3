e75e50ed14b714dc47304f8fe4b92d6f56fab0e0
FaridCompaore <farid-abdoulaye.compaore.1@etsmtl.net>
2024-06-27 21:49:37 -0400

    test reussi 2

--- test/test_.py
+++ test/test_.py
@@ -10,4 +10,8 @@ def test_subtraction():
     assert 5 - 3 == 2
 
 
+def test_addition():
+    assert 11 + 1 == 12
+
+
 # Ajoutez d'autres tests selon les fonctionnalités de votre application OxygenCS
